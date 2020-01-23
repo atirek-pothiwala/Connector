07d140ffa64fa9abf3b3613b11e11eaf3d42a422
Atirek Pothiwala <atirekpothiwala@gmail.com>
2020-01-23 22:00:09 +0530

    Migrate to X

--- connection/src/main/java/atirek/pothiwala/connection/Connector.java
+++ connection/src/main/java/atirek/pothiwala/connection/Connector.java
@@ -12,9 +12,10 @@ import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Environment;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.widget.SwipeRefreshLayout;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
 import android.util.Log;
 import android.webkit.MimeTypeMap;
 
